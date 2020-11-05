f56a35419809434df38f12ecb4da6c166a10fcfa
Lucas Zacarias <lucas21111999@gmail.com>
2020-11-05 15:18:44 -0300

    Estructura de Empleado como clase abstracta, junto a HashCode & Equals de su dni y legajo

--- 01-PB2-EDA2/src/cerveceria/Empleado.java
+++ 01-PB2-EDA2/src/cerveceria/Empleado.java
@@ -1,5 +1,75 @@
 package cerveceria;
 
-public class Empleado {
+public abstract class Empleado {
+		protected String nombre;
+		protected Integer dni;
+		protected Integer legajo;
+		protected Double sueldo;
+		
+		public abstract String enfocado_o_no_al_cliente();
 
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public Integer getDni() {
+			return dni;
+		}
+
+		public void setDni(Integer dni) {
+			this.dni = dni;
+		}
+
+		public Integer getLegajo() {
+			return legajo;
+		}
+
+		public void setLegajo(Integer legajo) {
+			this.legajo = legajo;
+		}
+
+		public Double getSueldo() {
+			return sueldo;
+		}
+
+		public void setSueldo(Double sueldo) {
+			this.sueldo = sueldo;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+			result = prime * result + ((legajo == null) ? 0 : legajo.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Empleado other = (Empleado) obj;
+			if (dni == null) {
+				if (other.dni != null)
+					return false;
+			} else if (!dni.equals(other.dni))
+				return false;
+			if (legajo == null) {
+				if (other.legajo != null)
+					return false;
+			} else if (!legajo.equals(other.legajo))
+				return false;
+			return true;
+		}
+		
+		
 }
